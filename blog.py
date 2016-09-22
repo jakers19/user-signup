a3ec90d6ede66b10f845e12604370f2389387755
jakers19 <jakers19@gmail.com>
2016-09-22 12:56:49 -0500

    finished user signup

--- blog.py
+++ blog.py
@@ -1,96 +1,89 @@
-import os
-import re
-from string import letters
-
 import webapp2
 import jinja2
+import os
+import re
+
 
 from google.appengine.ext import db
 
 start = """
-<a href="/blog">Let's Get Started</a>
+<a href="/unit2/signup">Sign Up Here</a>
 """
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
-                               autoescape = True)
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
 
 def render_str(template, **params):
     t = jinja_env.get_template(template)
     return t.render(params)
-
-class BlogHandler(webapp2.RequestHandler):
+    
+class BaseHandler(webapp2.RequestHandler):
+    def render(self, template, **kw):
+        self.response.out.write(render_str(template, **kw))
+        
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
+ 
+class MainPage(BaseHandler):
+    def get(self):
+        self.write("Let's Sign You Up!" + start)
 
-    def render_str(self, template, **params):
-        return render_str(template, **params)
 
-    def render(self, template, **kw):
-        self.write(self.render_str(template, **kw))
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and USER_RE.match(username)
 
-def render_post(response, post):
-    response.out.write('<b>' + post.subject + '</b><br>')
-    response.out.write(post.content)
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and PASS_RE.match(password)
 
-class MainPage(BlogHandler):
-  def get(self):
-      self.write('Hello, blogger!' + start)
+EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+def valid_email(email):
+    return not email or EMAIL_RE.match(email)
+      
+
+class SignUp(BaseHandler):
+
+    def get(self):
+        self.render("signup-form.html")
         
-  def post(self):
-      self.redirect('/blog/newpost')
-
-##### blog stuff
-
-def blog_key(name = 'default'):
-    return db.Key.from_path('blogs', name)
-
-class Post(db.Model):
-    subject = db.StringProperty(required = True)
-    content = db.TextProperty(required = True)
-    created = db.DateTimeProperty(auto_now_add = True)
-    last_modified = db.DateTimeProperty(auto_now = True)
-
-    def render(self):
-        self._render_text = self.content.replace('\n', '<br>')
-        return render_str("post.html", p = self)
-
-class BlogFront(BlogHandler):
-    def get(self):
-        posts = db.GqlQuery("select * from Post order by created desc limit 10")
-        self.render('front.html', posts = posts)
-
-class PostPage(BlogHandler):
-    def get(self, post_id):
-        key = db.Key.from_path('Post', int(post_id), parent=blog_key())
-        post = db.get(key)
-
-        if not post:
-            self.error(404)
-            return
-
-        self.render("permalink.html", post = post)
-
-class NewPost(BlogHandler):
-    def get(self):
-        self.render("newpost.html")
-
     def post(self):
-        subject = self.request.get('subject')
-        content = self.request.get('content')
-
-        if subject and content:
-            p = Post(parent = blog_key(), subject = subject, content = content)
-            p.put()
-            self.redirect('/blog/%s' % str(p.key().id()))
+        have_error = False
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify = self.request.get('verify')
+        email = self.request.get('email')
+        
+        params = dict(username = username, email = email)
+                 
+        if not valid_username(username):
+            params['error_username'] = "That's not a valid username."
+            have_error = True
+                               
+        if not valid_password(password):
+            params['error_password'] = "That wasn't a valid password."
+            have_error = True
+        elif password != verify:
+            params['error_verify'] = "Your passwords don't match."
+            have_error = True
+                     
+        if not valid_email(email):
+            params['error_email'] = "That is not a valid email address."
+            have_error = True
+                     
+        if have_error:
+            self.render('signup-form.html', **params)
         else:
-            error = "subject and content, please!"
-            self.render("newpost.html", subject=subject, content=content, error=error)
-
-
+            self.redirect('/unit2/welcome?username=' + username)
+                     
+class Welcome(BaseHandler):
+        def get(self):
+            username = self.request.get('username')
+            if valid_username(username):
+                 self.render('welcome.html', username = username)
+            else:
+                 self.redirect('/unit2/signup')
+                     
 app = webapp2.WSGIApplication([('/', MainPage),
-                               ('/blog/?', BlogFront),
-                               ('/blog/([0-9]+)', PostPage),
-                               ('/blog/newpost', NewPost),
-                               ],
-                              debug=True)
+                               ('/unit2/signup', SignUp),
+                               ('/unit2/welcome', Welcome)], debug = True)
